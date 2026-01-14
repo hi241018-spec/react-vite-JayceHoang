5aa1b0721c2697bd6bfc93957006e4484b71b531
Jayce Hoang <hi241018@gmail.com>
2026-01-14 20:24:32 +0700

    video28

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const MyComponent = () => {
+  return (
+    <div>Hoàng Xuân Dạtd</div>
+  );
+
+}
+
 const App = () => {
   const [count, setCount] = useState(0);
 
@@ -19,6 +26,7 @@ const App = () => {
         </a>
       </div>
       <h1>Hello World & JS Hoàng</h1>
+      <MyComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
