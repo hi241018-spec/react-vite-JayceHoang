d693f3b99dd2980b3287805759703ea254410ae5
Jayce Hoang <hi241018@gmail.com>
2026-01-21 21:07:17 +0700

    video 44

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import TodoData from './components/todo/TodoData';
 import TodoNew from './components/todo/TodoNew';
 import reactLogo from './assets/react.svg';
 import { useState } from 'react';
+import Header from './components/layout/header';
+import Footer from './components/layout/footer';
 const App = () => {
 
 
@@ -32,19 +34,23 @@ const App = () => {
     setTodoList(newTodo);
   }
   return (
-    <div className="todo-container">
-      <div className="todo-title"> Todo List</div>
-      <TodoNew
-        addNewTodo={addNewTodo}
-      />
-      <TodoData
-        todoList={todoList}
-        deleteTodo={deleteTodo}
-      />
-      <div className='todo-image'>
-        <img src={reactLogo} />
+    <>
+      <Header />
+      <div className="todo-container">
+        <div className="todo-title"> Todo List</div>
+        <TodoNew
+          addNewTodo={addNewTodo}
+        />
+        <TodoData
+          todoList={todoList}
+          deleteTodo={deleteTodo}
+        />
+        <div className='todo-image'>
+          <img src={reactLogo} />
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import LoginPage from './pages/login.jsx';
+import RegisterPage from './pages/register.jsx';
+import UserPage from './pages/user.jsx';
+import ProductsPage from './pages/product.jsx';
+import './styles/global.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/users",
+    element: <UserPage />
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/products",
+    element: <ProductsPage />
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
